06d39a963f5294d237c9b0c7d89f0155b59b81b3
fcturnerMTA <fcturner@mta.ca>
2021-07-10 21:52:11 -0300

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -5,6 +5,7 @@ from scipy import interpolate
 l= np.matrix([1.0,2.0,3.0,4.0,5.0,6.0,7.0])
 l= np.asarray(l)
 l= l.flatten()
+dl= np.min(np.abs(l - np.roll(l,1)))
   
 Il= np.matrix([0.0,0.1,0.3,0.6,1.0,1.5,2.1])
 Il= np.asarray(Il)
@@ -13,11 +14,9 @@ Il= Il.flatten()
 w= 1/l
 Iw=Il/w**2
 
-print(np.min(np.abs(w-np.roll(w,1))))
-print(w)
-res= np.min(np.abs(w - np.roll(w, 1)))
+wres= np.min(np.abs(w - np.roll(w, 1)))
 D= np.max(w) - np.min(w)
-Nfloat= np.ceil(D/res)
+Nfloat= np.ceil(D/wres)
 N= Nfloat.astype(int) 
 dw= D/Nfloat
 
@@ -35,11 +34,11 @@ Ie= lin(we)
 # print(np.sum(Ie)*dw)
 # print(np.sum(Ir)*dwr)
 
-
 plt.subplot(211)
 plt.plot(we,Ie)
 plt.subplot(212)
 plt.plot(l,Il)
 
-print(np.trapz(Ie, dx=dw))
-print(np.trapz(Il, dx=1))
+Ite= np.trapz(Ie, dx=dw)
+Itl= np.trapz(Il, dx=dl)
+print("Approximately", np.round(100/Itl*(Itl - Ite),1), "% Error between Wavenumber and Wavelength Integrals")
